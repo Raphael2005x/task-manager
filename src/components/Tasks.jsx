1edbda078f7c2af7274e0fc9b038535b444c06ed
Raphael <rapharesko@gmail.com>
2025-09-24 20:49:39 -0300

    adding functionality to the description button

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
-import { Check, ChevronRightIcon, Trash2, X } from "lucide-react"
+import { Check, ChevronRightIcon, Trash2, X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks = [], onTaskClicked, onDeleteTaskClick }) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set('title', task.title);
+        query.set('description', task.description);
+        navigate(`/task?${query.toString()}`)
+    }
+
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {tasks.map((task) => (
@@ -12,7 +22,7 @@ function Tasks({ tasks = [], onTaskClicked, onDeleteTaskClick }) {
                         {task.title}
 
                     </button>
-                    <button className="bg-slate-400 text-white p-2 rounded-md">
+                    <button onClick={() => onSeeDetailsClick(task)} className="bg-slate-400 text-white p-2 rounded-md">
                         <ChevronRightIcon />
                     </button>
                     <button onClick={() => onDeleteTaskClick(task.id)}
